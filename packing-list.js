a88d90ae97619728408b1dbc5c076d7fd1f0ee46
Yen <yen.hnguyen17@gmail.com>
2021-09-25 04:12:24 +0000

    Adding packing list by using while loop

--- packing-list.js
+++ packing-list.js
@@ -10,6 +10,8 @@ const packingList = [
 ];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
